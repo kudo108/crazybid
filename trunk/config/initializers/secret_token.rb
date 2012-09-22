5b89446c855cbeac1ce99e7d6a82e245f14ea2c4
ahn.willta@gmail.com <ahn.willta@gmail.com@765979a8-872c-f2cf-e81a-6608ebc80166>
2012-09-22 09:04:44 +0000

    Rails app skeleton

---
+++ trunk/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrazyBid::Application.config.secret_token = 'f9f466aaca36edd29ddc583a504d70fa8ccaf9da1b6ce67505a60904c489b3f21833b637c317a70a47db64f66ed97f974cd258c9653c326794ad6738df63cde9'
